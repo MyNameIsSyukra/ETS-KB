7bd78c24b551e2ecd46a734755a39f1cd173e2d4
MyNameIsSyukra <sukrawahyu@gmail.com>
2023-05-21 11:30:44 +0700

    update main

--- pac.py
+++ pac.py
@@ -482,4 +482,29 @@ class PacmanGameController:
                     self.reachable_spaces.append((y, x))
 
             self.numpy_maze.append(binary_row)
+
+
+""" if __name__ == "__main__":
+    unified_size = 32
+    pacman_game = PacmanGameController()
+    size = pacman_game.size
+    game_renderer = GameRenderer(size[0] * unified_size, size[1] * unified_size)
+
+    for y, row in enumerate(pacman_game.numpy_maze):
+        for x, column in enumerate(row):
+            if column == 0:
+                game_renderer.add_wall(Wall(game_renderer, x, y, unified_size))
+
+    for i, ghost_spawn in enumerate(pacman_game.ghost_spawns):
+        translated = translate_maze_to_screen(ghost_spawn)
+        ghost = Ghost(game_renderer, translated[0], translated[1], unified_size, pacman_game,
+                      pacman_game.ghost_colors[i % 4])
+        game_renderer.add_ghost(ghost)
+    
+    finish_line = FinishLine(game_renderer, size[0] - 2, size[1] - 2, unified_size)
+    game_renderer.add_finish_line(finish_line)
+
+    pacman = Hero(game_renderer, unified_size, unified_size, unified_size)
+    game_renderer.add_hero(pacman)
+    game_renderer.tick(120) """
     
